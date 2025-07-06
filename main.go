8e732f103df9bb8422d064251cbe5dc728e47f82
Alexey Plisov <alexplisov@gmail.com>
2025-07-06 16:41:49 +0500

    add console prints

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -11,7 +12,15 @@ func main() {
 	address := flag.String("address", "localhost:3000", "Specify address to run local server on.")
 	flag.Parse()
 
+	if *path == "" {
+		fmt.Println("No directory path specified.", "Specify directory path to host on local server.")
+		return
+	}
+
 	fsys := os.DirFS(*path)
 	handler := http.FileServerFS(fsys)
+
+	fmt.Printf("Local server for directory %s started on %s\n", *path, *address)
+
 	http.ListenAndServe(*address, handler)
 }
